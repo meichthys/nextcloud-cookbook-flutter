652e63ca47663e448af686eff28813c5bfdc32ad
meichthys <10717998+meichthys@users.noreply.github.com>
2021-12-19 22:27:02 -0500

    disable local notifications for now

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ void main() async {
   // Wait for Settings to be ready
   await Settings.init();
   // Wait for Notifications to be ready
-  await NotificationService().init();
+  // await NotificationService().init();
   runApp(
     LocalizedApp(
       delegate,
